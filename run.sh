4b19aeb439438bddd0576a06e0a244c73bc2c2d5
Shubham Minglani <shubham@linux.com>
2018-03-01 17:00:04 +0530

    fix command run

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 ./kedge apply -f configs/ --namespace $NAMESPACE
 oc expose service webdis --namespace $NAMESPACE
-WEBDIS_HOST=oc get route webdis --namespace $NAMESPACE --template={{.spec.host}}
+WEBDIS_HOST="$(oc get route webdis --namespace $NAMESPACE --template={{.spec.host}})"
 sh wait-for-it.sh $WEBDIS_HOST
 $WEBDIS_HOST python app.py
