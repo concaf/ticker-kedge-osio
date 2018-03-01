bafc2ff7d61f9443ec600d1c1a32d90c8bf1757b
Shubham Minglani <shubham@linux.com>
2018-03-01 17:02:24 +0530

    add port to host

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -e
 ./kedge apply -f configs/ --namespace $NAMESPACE
 oc expose service webdis --namespace $NAMESPACE || true
 WEBDIS_HOST="$(oc get route webdis --namespace $NAMESPACE --template={{.spec.host}})"
-sh wait-for-it.sh $WEBDIS_HOST
+sh wait-for-it.sh $WEBDIS_HOST:80
 $WEBDIS_HOST python app.py
