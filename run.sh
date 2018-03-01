9ca8a585f94b28ee3341534143a1a48b2d6d0cbc
Shubham Minglani <shubham@linux.com>
2018-03-01 17:01:10 +0530

    ignore expose error

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 ./kedge apply -f configs/ --namespace $NAMESPACE
-oc expose service webdis --namespace $NAMESPACE
+oc expose service webdis --namespace $NAMESPACE || true
 WEBDIS_HOST="$(oc get route webdis --namespace $NAMESPACE --template={{.spec.host}})"
 sh wait-for-it.sh $WEBDIS_HOST
 $WEBDIS_HOST python app.py
