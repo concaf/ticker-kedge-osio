0b0485c3893af5be3dd97c2ca4173c78890cd353
Shubham Minglani <shubham@linux.com>
2018-03-01 16:09:33 +0530

    add webdis and replaced redis

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from redis import Redis
+import urllib2
 import os
 
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+host = os.environ.get('WEBDIS_HOST', 'redis')
+
 
 @app.route('/')
 def hello():
-    visits = redis.incr('hits')
+    visits = urllib2.urlopen(host+"/INCR/hits").read()
 
     html = "<h3>Hello Kubernauts</h3> <br/>" \
            "<h2>Number of Hits:</2> {hits}<br/>"
 
     return html.format(hits=visits)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
