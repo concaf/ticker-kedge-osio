0ee7b1d1349780a2987284955e2f8d4892733ac4
Shubham Minglani <shubham@linux.com>
2018-03-01 16:46:18 +0530

    auto detect generate route host

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ set -x
 set -e
 
 ./kedge apply -f configs/ --namespace $NAMESPACE
+oc expose service webdis --namespace $NAMESPACE
+WEBDIS_HOST=oc get route webdis --namespace $NAMESPACE --template={{.spec.host}}
 sh wait-for-it.sh $WEBDIS_HOST
-python app.py
+$WEBDIS_HOST python app.py
